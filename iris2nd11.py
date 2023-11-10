0a32137deb497e131e1d408a570faa852d355468
OmarAltarakieh <144335241+OmarAltarakieh@users.noreply.github.com>
2023-11-10 12:18:33 +0100

    Update iris2nd11.py

--- iris2nd11.py
+++ iris2nd11.py
@@ -5,8 +5,6 @@ import sklearn
 from joblib import load
 
 # Load the trained model
-#with open('iris_knn_classifier2.pkl', 'rb') as f:
-    #model = pickle.load(f)
 model = load('iris_knn_classifier.joblib')
 # Main function for the Streamlit app
 def main():
